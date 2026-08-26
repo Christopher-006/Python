3c80c325ae0d292706dbdf16a1648ffbd5f80c83
Christopher-006 <ccruzperez@students.mchenry.edu>
2026-08-26 10:21:57 -0500

    Add files via upload

---
+++ joke.py
@@ -0,0 +1,3 @@
+print("My doctor told me im at risk of heart disease because I eat too much sodium")
+print("I took his advice with a grain of salt")
+# comment
---
+++ madlibs.py
@@ -0,0 +1,37 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included (Copy and paste THIS comment from opening to closing quotes).
+[ ] 2. Program asks for at least 5 different inputs (variables).
+[ ] 3. Output uses F-Strings to combine text and variables.
+[ ] 4. Output uses at least one escape sequence (\n or \t).
+[ ] 5. Code contains comments explaining the steps.
+[ ] 6. Program runs without errors.
+-----------------------------------------------------------------------
+"""
+
+
+
+
+
+
+# Get info from user 
+name = input("Please enter a person's name:")
+animal = input("Please enter a type of animal:")
+color = input("please enter a color")
+
+
+
+print(f"{name} has become friends with a {animal}")
+print(f"{animal} is {color}")
+print(f"{name} works at a sanctuary and that's how he met the {animal}")
+print(f"{animal} shares his exhibit with another {animal}")
+print(f"{name} takes care of the {animal} everyday")
+print(f"The second {animal} is a not {color}")
+print(f"{name} takes turns with someone else at the sanctuary")
+
+
+
+
+

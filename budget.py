94d1775ea8fa62cd469b844385fe0528caed78fa
Christopher-006 <chris081206@gmail.com>
2026-09-08 17:27:00 -0500

    Create budget.py
    Implemented monthly budget calculations including income input, expense tracking, and formatted output.

---
+++ budget.py
@@ -0,0 +1,23 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Ask user for Monthly Income (float).
+[ ] 3. Ask user for 5 DIFFERENT expense amounts (float)(Rent, Utilities, etc.)
+[ ] 4. Calculate Total Expenses and Remaining Balance.
+[ ] 5. Calculate Percentage of Income Spent.
+[ ] 6. Output formatted to 2 decimal places (:,.2f or :.2%).
+-----------------------------------------------------------------------
+"""
+# --- 1. GET INPUT ---
+gross_pay = float(input("2,283.67: $"))
+
+# --- 2. CALCULATIONS ---
+456.73 = 2,283.67 * 0.20
+1,826.94 = 2,283.67 - 456.73
+
+# --- 3. FORMATTED OUTPUT ---
+print(f"Gross Pay:      ${gross_pay:,.2f}")
+print(f"Net Pay:        ${net_pay:,.2f}")
+# comment

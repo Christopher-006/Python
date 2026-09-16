7bb595aa6654d6b79dcb9d337e5fd8b688a593c4
Christopher-006 <chris081206@gmail.com>
2026-09-16 10:31:43 -0500

    Update atm.py
    Refactor menu choice handling and improve user input.

--- atm.py
+++ atm.py
@@ -9,7 +9,6 @@ Thinking CRUD
 💡When we get to crud, we will present users with a menu of choices. This is a standard interface module. Now that we know Match Case, it makes menu choices easy. It will get even easier when we get to functions.
 """
 
-
 # Match case for menu
 # Display menu
 print(f" 1.  Create a new contact")
@@ -19,23 +18,18 @@ print(f" 4.  Delete a contact")
 print(f" 5.  Quit")
 
 # get user choice
-
-choice = int(input("Please enter the number of your selection:  "))
-
+choice = 1
 while choice > 0 and choice < 4:
-    # Create new contacts
+    choice = int(input("Please enter the number of your selection:  "))
     match choice:
-
 
         case 1:
             print("Create")
         case 2:
             print("Search")  # read
-        # Delete contact
-        case 3: 
+        case 3:
             print("Update")
-        case 4: 
+        case 4:
             print("Delete")
         case 5:
             print("Good bye!")
-

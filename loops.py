63a7070a651717db18ef8394dfe55fc7b113ce75
Christopher-006 <chris081206@gmail.com>
2026-09-21 10:30:43 -0500

    Add files via upload

--- loops.py
+++ loops.py
@@ -8,16 +8,16 @@ ASSIGNMENT REQUIREMENTS
        - Uses a boolean variable to control the loop.
 [ ] 3. Task 2: For Loop (99 Bottles of Beer)
        - Counts backwards from 99 to 1.
-       - Prints the full stanza with the correct number of bottles, changes to 1 bottle (no s) when needed
+       - Prints "[number] bottles of beer on the wall!"
 [ ] 4. Upload to GitHub and paste the link below.
 -----------------------------------------------------------------------
 """
 # range(start, stop, step)
-for i in range(4, 0, -1):
+for i in range(10, 0, -1):
     print(f"{i}...")
 
 print("🐱 A wild cat appears!")
 print("   /\\_/\\")
 print("  ( o.o )")
 print("   > ^ <")
-#comment you gave me an extension
+# comment you gave me an extension

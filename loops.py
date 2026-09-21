31f66731c391715198a6722e8a16111b40ea15fd
Christopher-006 <chris081206@gmail.com>
2026-09-21 10:24:04 -0500

    Create loops.py
    Added a header docstring outlining assignment requirements and tasks. Included a while loop for user input and a for loop counting down from 99.

---
+++ loops.py
@@ -0,0 +1,23 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Task 1: While Loop (The Nagging Kid)
+       - Repeats "Are we there yet?" until user types "yes".
+       - Uses a boolean variable to control the loop.
+[ ] 3. Task 2: For Loop (99 Bottles of Beer)
+       - Counts backwards from 99 to 1.
+       - Prints the full stanza with the correct number of bottles, changes to 1 bottle (no s) when needed
+[ ] 4. Upload to GitHub and paste the link below.
+-----------------------------------------------------------------------
+"""
+# range(start, stop, step)
+for i in range(4, 0, -1):
+    print(f"{i}...")
+
+print("🐱 A wild cat appears!")
+print("   /\\_/\\")
+print("  ( o.o )")
+print("   > ^ <")
+#comment you gave me an extension

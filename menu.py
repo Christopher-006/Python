ba46aeb67a90bb05eb8c8f8f155ba7edb300f366
Christopher-006 <chris081206@gmail.com>
2026-09-23 10:05:09 -0500

    Add files via upload

--- menu.py
+++ menu.py
@@ -1,13 +1,4 @@
-"""
-Thinking CRUD
 
-🏗️  Create
-📖  Read
-🔃  Update
-🗑️  Delete
-
-💡When we get to crud, we will present users with a menu of choices. This is a standard interface module. Now that we know Match Case, it makes menu choices easy. It will get even easier when we get to functions.
-"""
 
 
 # initialize 1000.00
@@ -26,16 +17,21 @@ while choice > 0 and choice < 6:
 
         case 1:
             print(f"Your current balance is: {balance}")
+            continue
         case 2:
             deposit_amount = float(input("Enter deposit amount: "))
             print(f"Get deposit amt -> Validate it is numeric -> Add to balance: {deposit_amount}")
+            continue
         case 3:
             withdraw_amount = float(input("Enter withdraw amount: "))
             print(f"Get withdraw amt -> Validate it is numeric -> Check for Overdraft -> Subtract: {withdraw_amount}")
+            continue
         case 4:
             transfer_amount = float(input("Enter transfer amount: "))
             print(f"Get transfer amt -> Validate it is numeric -> Check for Overdraft -> Subtract: {transfer_amount}")
+            continue
         case 5:
             print(f"Print goodbye -> Break loop")
+            break
         case _:
             print(f"Invalid Selection")
